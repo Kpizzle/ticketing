66948d3dc50a262730591fe38a4497b9493411d3
Kyle Potter <kp1289@gmail.com>
2024-01-24 23:38:08 +0000

    added funcationality to call auth service from next server

--- client/pages/auth/signup.jsx
+++ client/pages/auth/signup.jsx
@@ -1,22 +1,24 @@
 import { useState } from 'react';
-import axios from 'axios';
+import Router from 'next/router';
+import useRequest from '../../hooks/use-request';
 
 const signUp = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [errors, setErrors] = useState([]);
+  const { doRequest, errors } = useRequest({
+    url: '/api/users/signup',
+    method: 'post',
+    body: {
+      email,
+      password,
+    },
+    onSuccess: () => Router.push('/'),
+  });
 
   const onSubmit = async (event) => {
     event.preventDefault();
-    try {
-      const response = await axios.post('/api/users/signup', {
-        email,
-        password,
-      });
-      console.log(response.data);
-    } catch (error) {
-      setErrors(error.response.data.errors);
-    }
+
+    await doRequest();
   };
 
   return (
@@ -40,9 +42,7 @@ const signUp = () => {
           className='form-control'
         />
       </div>
-      {errors.map((err) => {
-        return err.message;
-      })}
+      {errors}
       <button className='btn btn-primary'>Sign Up</button>
     </form>
   );
--- client/pages/index.jsx
+++ client/pages/index.jsx
@@ -1,4 +1,7 @@
-const index = () => {
+import axios from 'axios';
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
   return (
     <div>
       <h1>Index</h1>
@@ -6,4 +9,21 @@ const index = () => {
   );
 };
 
-export default index;
+LandingPage.getInitialProps = async ({ req }) => {
+  if (typeof window === 'undefined') {
+    const { data } = await axios.get(
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+      {
+        headers: req.headers,
+      }
+    );
+    return data;
+  } else {
+    const { data } = await axios.get('/api/users/currentuser').catch((err) => {
+      console.log(err);
+    });
+    return data;
+  }
+};
+
+export default LandingPage;
